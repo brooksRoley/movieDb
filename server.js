e119d05ef6df2bf35ad56820b859205d18281944
zero400 <brooksroley@gmail.com>
2019-05-07 20:19:08 -0700

    update backend routes

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = '808cabea1582db02810d3c942e6781f8';
 
 // Get Popular
-app.get('/api/popular', (req, res) => {
-  request(`${BASE_URL}/movie/popular?api_key=${API_KEY}`, (err, response, body) => {
+app.get('/api/popular/:page', (req, res) => {
+  let page = req.params.page || 1;
+  request(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`, (err, response, body) => {
     if (err) { return console.log(err); }
     res.send(body);
   })
@@ -37,11 +38,37 @@ app.get('/api/movie/:id', (req, res) => {
     res.send(body);
   })
 })
+// Get Similar Movies
+app.get('/api/movie/:id/similar', (req, res) => {
+  let id = req.params.id;
+  request(`${BASE_URL}/movie/${id}/similar?api_key=${API_KEY}`, (err, response, body) => {
+    if (err) { return console.log(err); }
+    res.send(body);
+  })
+})
 
 // Get Cast
 app.get('/api/cast/:id', (req, res) => {
   let id = req.params.id;
   request(`${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}`, (err, response, body) => {
+    if (err) { return console.log(err); }
+    res.send(body);
+  })
+})
+
+// Get Person
+app.get('/api/person/:id', (req, res) => {
+  let id = req.params.id;
+  request(`${BASE_URL}/person/${id}?api_key=${API_KEY}`, (err, response, body) => {
+    if (err) { return console.log(err); }
+    res.send(body);
+  })
+})
+
+// Get Person's Credits
+app.get('/api/person/:id/credits', (req, res) => {
+  let id = req.params.id;
+  request(`${BASE_URL}/person/${id}/movie_credits?api_key=${API_KEY}`, (err, response, body) => {
     if (err) { return console.log(err); }
     res.send(body);
   })
